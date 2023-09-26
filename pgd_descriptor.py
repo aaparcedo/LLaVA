3289671111e1c92f62d6d5ae7c6397d4b629233e
aaparcedo <aaparcedo.io@gmail.com>
2023-09-25 22:47:02 -0400

    fix imagenet dir file

--- pgd_descriptor.py
+++ pgd_descriptor.py
@@ -616,7 +616,7 @@ def boolean_string(s):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("--image_name", type=str, default="zebra.jpeg")
-    parser.add_argument("--image_list", type=str, default="./imagenet_test.txt.txt")
+    parser.add_argument("--image_list", type=str, default="./imagenet_test.txt")
     parser.add_argument("--label_list", type=str, default="./coco_label.json")
     parser.add_argument("--attack_type", type=str, default="pgd")
     parser.add_argument("--set", type=int, required=True)
